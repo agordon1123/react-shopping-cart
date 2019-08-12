60e820a072dedc3ac8b8191c1e76a9cc82e0f0cb
Alex <agordon1123@gmail.com>
2019-08-12 15:49:17 -0400

    step 5 complete

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext';
 
 // Components
 import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
+const ShoppingCart = () => {
+	const cart = useContext(CartContext);
+	
 	const getCartTotal = () => {
 		return props.cart.reduce((acc, value) => {
 			return acc + value.price;
@@ -12,7 +15,7 @@ const ShoppingCart = props => {
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
+			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
 
